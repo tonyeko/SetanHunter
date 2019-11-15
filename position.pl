3ee6a3dcf6c95d6ae3e593f98564d4ba4fea800b
Neptune3X <52845093+Neptune3X@users.noreply.github.com>
2019-11-15 14:12:12 +0700

    Penambahan lokasi
    Jika ada yang ingin diganti, silahkan comment

--- position.pl
+++ position.pl
@@ -12,6 +12,11 @@
     pos(1,8, 'labtek V.').
     pos(1,9, 'labtek V.').
     pos(1,10, 'labtek V.').
+    pos(1,11, 'labtek VI.').
+    pos(1,12, 'labtek VI.').
+    pos(1,13, 'labtek VI.').
+    pos(1,14, 'Aula Barat.').
+    pos(1,15, 'Aula Barat.').
     pos(2,1, 'lapbas itb.').
     pos(2,2, 'lapbas itb.').
     pos(2,3, 'lapbas itb.').
@@ -22,6 +27,11 @@
     pos(2,8, 'labtek V.').
     pos(2,9, 'labtek V.').
     pos(2,10, 'labtek V.').
+    pos(2,11, 'Aula Timur.').
+    pos(2,12, 'Aula Timur.').
+    pos(2,13, 'Aula Timur').
+    pos(2,14, 'saraga.').
+    pos(2,15, 'saraga.').
     pos(3,1, 'jalan.').
     pos(3,2, 'jalan.').
     pos(3,3, 'lapbas itb.').
@@ -32,6 +42,11 @@
     pos(3,8, 'labtek V.').
     pos(3,9, 'labtek V.').
     pos(3,10, 'labtek V.').
+    pos(3,11, 'labtek II.').
+    pos(3,12, 'labtek II.').
+    pos(3,13, 'Kansar.').
+    pos(3,14, 'CC Barat.').
+    pos(3,15, 'CC Barat.').
     pos(4,1, 'jalan.').
     pos(4,2, 'jalan.').
     pos(4,3, 'lapbas itb.').
@@ -42,6 +57,11 @@
     pos(4,8, 'labtek V.').
     pos(4,9, 'labtek V.').
     pos(4,10, 'labtek V.').
+    pos(4,11, 'CC Timur.').
+    pos(4,12, 'CC Timur.').
+    pos(4,13, 'Lab Basis Data.').
+    pos(4,14, 'Lab Basis Data.').
+    pos(4,15, 'Lab Basis Data.').
     pos(5,1, 'jalan.').
     pos(5,2, 'jalan.').
     pos(5,3, 'lapbas itb.').
@@ -52,36 +72,56 @@
     pos(5,8, 'labtek V.').
     pos(5,9, 'labtek V.').
     pos(5,10, 'labtek V.').
+    pos(5,11, 'labtek I.').
+    pos(5,12, 'labtek I.').
+    pos(5,13, 'labtek III.').
+    pos(5,14, 'labtek III.').
+    pos(5,15, 'Aula Timur.').
     pos(6,1, 'jalan.').
     pos(6,2, 'jalan.').
     pos(6,3, 'jalan.').
     pos(6,4, 'jalan.').
-    pos(6,5, 'jalan.').
-    pos(6,6, 'jalan.').
-    pos(6,7, 'jalan.').
-    pos(6,8, 'labtek V.').
-    pos(6,9, 'labtek V.').
-    pos(6,10, 'labtek V.').
+    pos(6,5, 'Mall Ciwalk.').
+    pos(6,6, 'Mall Ciwalk.').
+    pos(6,7, 'Mall Ciwalk.').
+    pos(6,8, 'labtek VIII.').
+    pos(6,9, 'labtek VIII.').
+    pos(6,10, 'labtek VIII.').
+    pos(6,11, 'Lab Kimia.').
+    pos(6,12, 'Lab Kimia.').
+    pos(6,13, 'Lab Fisika.').
+    pos(6,14, 'Lab Fisika.').
+    pos(6,15, 'Monument.').
     pos(7,1, 'jalan.').
     pos(7,2, 'jalan.').
-    pos(7,3, 'jalan.').
-    pos(7,4, 'jalan.').
+    pos(7,3, 'Sadikin.').
+    pos(7,4, 'Sadikin.').
     pos(7,5, 'jalan.').
     pos(7,6, 'jalan.').
-    pos(7,7, 'jalan.').
-    pos(7,8, 'jalan.').
+    pos(7,7, 'Kuil McD.').
+    pos(7,8, 'Kuil McD.').
     pos(7,9, 'jalan.').
     pos(7,10, 'jalan.').
+    pos(7,11, 'labtek IV.').
+    pos(7,12, 'labtek IV.').
+    pos(7,13, 'labtek IV.').
+    pos(7,14, 'Aula Barat.').
+    pos(7,15, 'Aula Barat.').
     pos(8,1, 'jalan.').
     pos(8,2, 'jalan.').
     % pos(8,3, 'jalan.').
     pos(8,4, 'jalan.').
     pos(8,5, 'jalan.').
-    pos(8,6, 'jalan.').
-    pos(8,7, 'jalan.').
+    pos(8,6, 'Gudang Kebun.').
+    pos(8,7, 'Gudang Kebun.').
     pos(8,8, 'jalan.').
     pos(8,9, 'kolam intel.').
     pos(8,10, 'kolam intel.').
+    pos(8,11, 'Crisbar.').
+    pos(8,12, 'Crisbar.').
+    pos(8,13, 'UKJ.').
+    pos(8,14, 'UKJ.').
+    pos(8,15, 'UKJ.').
     pos(9,1, 'jalan.').
     pos(9,2, 'jalan.').
     pos(9,3, 'lapcin itb.').
@@ -92,6 +132,11 @@
     pos(9,8, 'jalan.').
     pos(9,9, 'kolam intel.').
     pos(9,10, 'kolam intel.').
+    pos(9,11, 'Genshiken.').
+    pos(9,12, 'Genshiken.').
+    pos(9,13, 'Sekre I.').
+    pos(9,14, 'Sekre II.').
+    pos(9,15, 'Sekre III.').
     pos(10,1, 'parkiran sr.').
     pos(10,2, 'parkiran sr.').
     pos(10,3, 'lapcin itb.').
@@ -102,6 +147,87 @@
     pos(10,8, 'jalan.').
     pos(10,9, 'jalan.').
     pos(10,10, 'jalan.').
+    pos(10,11, 'PLN.').
+    pos(10,12, 'PLN.').
+    pos(10,13, 'PLN.').
+    pos(10,14, 'Trek lari.').
+    pos(10,15, 'Trek lari.').
+    pos(11,1, 'parkiran sr.').
+    pos(11,2, 'parkiran sr.').
+    pos(11,3, 'jalan').
+    pos(11,4, 'jalan.').
+    pos(11,5, 'jalan.').
+    pos(11,6, 'Upnormal Sumur.').
+    pos(11,7, 'Upnormal Sumur.').
+    pos(11,8, 'jalan.').
+    pos(11,9, 'jalan.').
+    pos(11,10, 'jalan.').
+    pos(11,11, 'Koperasi.').
+    pos(11,12, 'Koperasi.').
+    pos(11,13, 'Koperasi.').
+    pos(11,14, 'Trek lari.').
+    pos(11,15, 'Trek lari.').
+    pos(12,1, 'Oktagon.').
+    pos(12,2, 'Oktagon.').
+    pos(12,3, 'Mall Istana Plaza.').
+    pos(12,4, 'Mall Istana Plaza.').
+    pos(12,5, 'Mall Istana Plaza.').
+    pos(12,6, 'jalan.').
+    pos(12,7, 'jalan.').
+    pos(12,8, 'jalan.').
+    pos(12,9, 'Kandang Domba.').
+    pos(12,10, 'Kandang Domba.').
+    pos(12,11, 'Kandang Domba.').
+    pos(12,12, 'Rest Area.').
+    pos(12,13, 'Sabuga.').
+    pos(12,14, 'Sabuga.').
+    pos(12,15, 'Sabuga.').
+    pos(13,1, 'TVST.').
+    pos(13,2, 'TVST.').
+    pos(13,3, 'Monument.').
+    pos(13,4, 'Monument.').
+    pos(13,5, 'Monument.').
+    pos(13,6, 'jalan.').
+    pos(13,7, 'jalan.').
+    pos(13,8, 'jalan.').
+    pos(13,9, 'Tamfest.').
+    pos(13,10, 'Tamfest.').
+    pos(13,11, 'Tamfest.').
+    pos(13,12, 'Rest Area.').
+    pos(13,13, 'Lab IRK.').
+    pos(13,14, 'Lab IRK.').
+    pos(13,15, 'Lab IRK.').
+    pos(14,1, 'Perpustakaan.').
+    pos(14,2, 'Perpustakaan.').
+    pos(14,3, 'Jalan.').
+    pos(14,4, 'Jalan.').
+    pos(14,5, 'Anguish of Darkness.').
+    pos(14,6, 'jalan.').
+    pos(14,7, 'jalan.').
+    pos(14,8, 'jalan.').
+    pos(14,9, 'Anguish of Darkness.').
+    pos(14,10, 'Camp.').
+    pos(14,11, 'Camp.').
+    pos(14,12, 'Rest Area.').
+    pos(14,13, 'Lab SI.').
+    pos(14,14, 'Lab SI.').
+    pos(14,15, 'Lab SI.').
+    pos(15,1, 'Perpustakaan.').
+    pos(15,2, 'Perpustakaan.').
+    pos(15,3, 'Jalan.').
+    pos(15,4, 'Jalan.').
+    pos(15,5, 'RS Boromeus.').
+    pos(15,6, 'jalan.').
+    pos(15,7, 'jalan.').
+    pos(15,8, 'RS Boromeus.').
+    pos(15,9, 'Road to Everywhere.').
+    pos(15,10, 'Jalan.').
+    pos(15,11, 'Jalan.').
+    pos(15,12, 'Road to Everywhere.').
+    pos(15,13, 'Castle of Anguish.').
+    pos(15,14, 'Castle of Anguish.').
+    pos(15,15, 'Castle of Anguish.').
+
 
 /* Batas Pagar */
     /* Baris ke-0 */
