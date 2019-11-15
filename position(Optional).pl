a57ace1e993cd6513e760038de5f7b5951f1e2d2
Neptune3X <52845093+Neptune3X@users.noreply.github.com>
2019-11-15 17:07:18 +0700

    nama posisi tiap koordinar di ubah semua

---
+++ position(Optional).pl
@@ -0,0 +1,307 @@
+/* Santuary Position */
+    sanctuary(5,7).
+    sanctuary(11,12).
+    
+/*Realm if Seven Sins*/
+/* Location Name */
+    pos(1,1, 'Graveyard.').
+    pos(1,2, 'Graveyard.').
+    pos(1,3, 'Road of Hell.').
+    pos(1,4, 'Road of Hell.').
+    pos(1,5, 'Spike Mountain.').
+    pos(1,6, 'Road.').
+    pos(1,7, 'Road.').
+    pos(1,8, 'Spring of Gluttony.').
+    pos(1,9, 'Spring of Gluttony.').
+    pos(1,10, 'Spring of Gluttony.').
+    pos(1,11, 'Road.').
+    pos(1,12, 'Road.').
+    pos(1,13, 'Castle of Hunger.').
+    pos(1,14, 'Castle of Hunger.').
+    pos(1,15, 'Aurora Garden.').
+    pos(2,1, 'Graveyard.').
+    pos(2,2, 'City of Harbringer.').
+    pos(2,3, 'City of Harbringer.').
+    pos(2,4, 'Road.').
+    pos(2,5, 'Spike Mountain.').
+    pos(2,6, 'Swamp of Hate.').
+    pos(2,7, 'Swamp of Hate.').
+    pos(2,8, 'Swamp of Hate.').
+    pos(2,9, 'Mountain of the Dead.').
+    pos(2,10, 'Mining Torture.').
+    pos(2,11, 'Mining Torture.').
+    pos(2,12, 'Akihabara Village.').
+    pos(2,13, 'Akihabara Village').
+    pos(2,14, 'Despair City.').
+    pos(2,15, 'Despair City.').
+    pos(3,1, 'Road.').
+    pos(3,2, 'Frisky Cliff.').
+    pos(3,3, 'Frisky Cliff.').
+    pos(3,4, 'Road.').
+    pos(3,5, 'Spike Mountain.').
+    pos(3,6, 'Dragon Den.').
+    pos(3,7, 'Dragon Den.').
+    pos(3,8, 'City of Dream.').
+    pos(3,9, 'Dreaming Mountain.').
+    pos(3,10, 'Dreaming Mountain.').
+    pos(3,11, 'Path to Dream.').
+    pos(3,12, 'Path to Dream.').
+    pos(3,13, 'Castle of Sloth.').
+    pos(3,14, 'Castle of Sloth.').
+    pos(3,15, 'Garden of Everlasting Dream.').
+    pos(4,1, 'Tunnel of Despair.').
+    pos(4,2, 'Forest of Unluckyness.').
+    pos(4,3, 'Forest of Unluckyness.').
+    pos(4,4, 'Road.').
+    pos(4,5, 'Road.').
+    pos(4,6, 'Treasure Town.').
+    % pos(4,7, 'Road.').
+    pos(4,8, 'Lost Forest.').
+    pos(4,9, 'Lost Forest.').
+    pos(4,10, 'Gate of Soul.').
+    pos(4,11, 'Gate of Soul.').
+    pos(4,12, 'Soul City.').
+    pos(4,13, 'Road.').
+    pos(4,14, 'Road.').
+    pos(4,15, 'Soul Cave.').
+    pos(5,1, 'Tunnel of Despair.').
+    pos(5,2, 'False Hope City.').
+    pos(5,3, 'False Hope City.').
+    pos(5,4, 'Room of Chaos.').
+    pos(5,5, 'Room of Chaos.').
+    pos(5,6, 'Chaos Garden.').
+    pos(5,7, 'Santuary.').
+    pos(5,8, 'Road.').
+    pos(5,9, 'Spring of Eternality.').
+    pos(5,10, 'Spring of Eternality.').
+    pos(5,11, 'Road.').
+    pos(5,12, 'Road.').
+    pos(5,13, 'Castle of Greed.').
+    pos(5,14, 'Castle of Greed.').
+    pos(5,15, 'Memoir la Encore.').
+    pos(6,1, 'Road.').
+    pos(6,2, 'Road.').
+    pos(6,3, 'Rebellion Camp.').
+    pos(6,4, 'Rebellion Camp.').
+    pos(6,5, 'City of Unforgiveness.').
+    pos(6,6, 'City of Unforgiveness.').
+    pos(6,7, 'Road.').
+    pos(6,8, 'Road.').
+    pos(6,9, 'Path to Destruction.').
+    pos(6,10, 'Path to Destruction.').
+    pos(6,11, 'Path to Destruction.').
+    pos(6,12, 'Nightmare City.').
+    pos(6,13, 'Road.').
+    pos(6,14, 'Wasteland of Undead.').
+    pos(6,15, 'Wasteland of Undead.').
+    pos(7,1, 'Blank City.').
+    pos(7,2, 'Blank City.').
+    pos(7,3, 'Path of Lust.').
+    pos(7,4, 'Path of Lust.').
+    pos(7,5, 'Road.').
+    pos(7,6, 'Road.').
+    pos(7,7, 'Castle of Lust.').
+    pos(7,8, 'Castle of Lust.').
+    pos(7,9, 'Garden of Agony.').
+    pos(7,10, 'Spring og Agony.').
+    pos(7,11, 'Demon Path.').
+    pos(7,12, 'Demon Path.').
+    pos(7,13, 'Overlord Castle.').
+    pos(7,14, 'Overlord Castle.').
+    pos(7,15, 'Overlord Castle.').
+    pos(8,1, 'Blank City.').
+    pos(8,2, 'Road.').
+    % pos(8,3, 'Road.').
+    pos(8,4, 'Grief Ocean.').
+    pos(8,5, 'Grief Ocean.').
+    pos(8,6, 'Road.').
+    pos(8,7, 'Road.').
+    pos(8,8, 'Slayers Domain.').
+    pos(8,9, 'Slayers Domain.').
+    pos(8,10, 'Road.').
+    pos(8,11, 'Demon Path.').
+    pos(8,12, 'Demon Path.').
+    pos(8,13, 'Overlord Castle.').
+    pos(8,14, 'Overlord Castle.').
+    pos(8,15, 'Overlord Castle.').
+    pos(9,1, 'Road.').
+    pos(9,2, 'Forest of Jealousy.').
+    pos(9,3, 'Forest of Jealousy.').
+    pos(9,4, 'Castle of Envy.').
+    pos(9,5, 'Castle of Envy.').
+    pos(9,6, 'Hateful Garden.').
+    pos(9,7, 'Road.').
+    pos(9,8, 'Road.').
+    pos(9,9, 'Styx River.').
+    pos(9,10, 'Styx River.').
+    pos(9,11, 'Vitality City.').
+    pos(9,12, 'Vitality City.').
+    pos(9,13, 'Dreadful Zone.').
+    pos(9,14, 'Dreadful Zone.').
+    pos(9,15, 'Dreadful Zone.').
+    pos(10,1, 'Dead or Alive.').
+    pos(10,2, 'Dead or Alive.').
+    pos(10,3, 'Road.').
+    pos(10,4, 'Road.').
+    pos(10,5, 'Sorrowful City.').
+    pos(10,6, 'Sorrowful City.').
+    pos(10,7, 'Path of Crying.').
+    pos(10,8, 'Path of Crying.').
+    pos(10,9, 'Road.').
+    pos(10,10, 'Road.').
+    pos(10,11, 'Wasteland of Project Hell.').
+    pos(10,12, 'Wasteland of Project Hell.').
+    pos(10,13, 'Wasteland of Project Hell.').
+    pos(10,14, 'School of No-Hope.').
+    pos(10,15, 'School of No-Hope.').
+    pos(11,1, 'Road.').
+    pos(11,2, 'Menace City.').
+    pos(11,3, 'Menace City').
+    pos(11,4, 'Path to Glory.').
+    pos(11,5, 'Path to Glory.').
+    pos(11,6, 'Path to Glory.').
+    pos(11,7, 'Swamp of Pride.').
+    pos(11,8, 'Swamp of Pride.').
+    pos(11,9, 'Castle of Pride.').
+    pos(11,10, 'Castle of Pride.').
+    pos(11,11, 'Road.').
+    % pos(11,12, 'Sanctuary.').
+    pos(11,13, 'Road.').
+    pos(11,14, 'Running Zombie Zone.').
+    pos(11,15, 'Running Zombie Zone.').
+    pos(12,1, 'Screaming Mine.').
+    pos(12,2, 'Screaming Mine.').
+    pos(12,3, 'Road.').
+    pos(12,4, 'Road.').
+    pos(12,5, 'Never-Ending Regret.').
+    pos(12,6, 'Never-Ending Regret.').
+    pos(12,7, 'Never-Ending Regret.').
+    pos(12,8, 'Road.').
+    pos(12,9, 'Dreadful Village.').
+    pos(12,10, 'Dreadful Village.').
+    pos(12,11, 'Forest of the Lost.').
+    pos(12,12, 'Forest of the Lost.').
+    pos(12,13, 'Mandrake City.').
+    pos(12,14, 'Mandrake Farm.').
+    pos(12,15, 'Mandrake Farm.').
+    pos(13,1, 'Castle of Wrath.').
+    pos(13,2, 'Castle of Wrath.').
+    pos(13,3, 'Crown of Authority.').
+    pos(13,4, 'Crown of Authority.').
+    pos(13,5, 'Monument of Grieving.').
+    pos(13,6, 'Monument of Grieving.').
+    pos(13,7, 'Road.').
+    pos(13,8, 'Road.').
+    pos(13,9, 'Wasteland.').
+    pos(13,10, 'Wasteland.').
+    pos(13,11, 'Grimoire of Despair.').
+    pos(13,12, 'Grimoire of Ancient.').
+    pos(13,13, 'Grimoire of Lust.').
+    pos(13,14, 'Grimoire of Dream.').
+    pos(13,15, 'Grimoire of Destruction.').
+    pos(14,1, 'Chaos Library.').
+    pos(14,2, 'Chaos Library.').
+    pos(14,3, 'Road.').
+    pos(14,4, 'Road.').
+    pos(14,5, 'Untruthful City.').
+    pos(14,6, 'Untruthful City.').
+    pos(14,7, 'Monument of Betrayal.').
+    pos(14,8, 'Monument of Betrayal.').
+    pos(14,9, 'Road.').
+    pos(14,10, 'Road.').
+    pos(14,11, 'Anguish of Darkness.').
+    pos(14,12, 'Anguish of Darkness.').
+    pos(14,13, 'Soulstroke Hot Spring.').
+    pos(14,14, 'Soulstroke Hot Spring.').
+    pos(14,15, 'Soulstroke Hot Spring.').
+    pos(15,1, 'Breakdown City.').
+    pos(15,2, 'Breakdown City.').
+    pos(15,3, 'Road.').
+    pos(15,4, 'Road.').
+    pos(15,5, 'Hospital of Agony.').
+    pos(15,6, 'Dead Trial.').
+    pos(15,7, 'Hospital of Agony.').
+    pos(15,8, 'RS Boromeus.').
+    pos(15,9, 'Path to Nowhere.').
+    pos(15,10, 'Road.').
+    pos(15,11, 'Road.').
+    pos(15,12, 'Path to Nowhere.').
+    pos(15,13, 'Castle of Anguish.').
+    pos(15,14, 'Castle of Anguish.').
+    pos(15,15, 'Sinful Garden.').
+
+
+/* Batas Pagar */
+    /* Baris ke-0 */
+    hedge(0,0).
+    hedge(0,1).
+    hedge(0,2).
+    hedge(0,3).
+    hedge(0,4).
+    hedge(0,5).
+    hedge(0,6).
+    hedge(0,7).
+    hedge(0,8).
+    hedge(0,9).
+    hedge(0,10).
+    hedge(0,11).
+    hedge(0,12).
+    hedge(0,13).
+    hedge(0,14).
+    hedge(0,15).
+    hedge(0,16).
+    /* Baris ke-16 */
+    hedge(16,1).
+    hedge(16,2).
+    hedge(16,3).
+    hedge(16,4).
+    hedge(16,5).
+    hedge(16,6).
+    hedge(16,7).
+    hedge(16,8).
+    hedge(16,9).
+    hedge(16,10).
+    hedge(16,11).
+    hedge(16,12).
+    hedge(16,13).
+    hedge(16,14).
+    hedge(16,15).
+    hedge(16,16).
+    /* Kolom ke-0 */
+    hedge(1,0).
+    hedge(2,0).
+    hedge(3,0).
+    hedge(4,0).
+    hedge(5,0).
+    hedge(6,0).
+    hedge(7,0).
+    hedge(8,0).
+    hedge(9,0).
+    hedge(10,0).
+    hedge(11,0).
+    hedge(12,0).
+    hedge(13,0).
+    hedge(14,0).
+    hedge(15,0).
+    hedge(16,0).
+    /* Kolom ke-11 */
+    hedge(1,16).
+    hedge(2,16).
+    hedge(3,16).
+    hedge(4,16).
+    hedge(5,16).
+    hedge(6,16).
+    hedge(7,16).
+    hedge(8,16).
+    hedge(9,16).
+    hedge(10,16).
+    hedge(11,16).
+    hedge(12,16).
+    hedge(13,16).
+    hedge(14,16).
+    hedge(15,16).
+    hedge(16,16).
+
+    /* Random Hedge */
+    hedge(4,7).
+    hedge(8,3).
